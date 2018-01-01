7188cd90bb5e0c7a085ca487c9578ab9e6b78505
Kyle Parisi <kyleparisi@gmail.com>
2018-01-01 17:27:29 -0500

    Add nocache headers to auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,25 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 
-router.get("/status", function(req, res) {
+function nocache(req, res, next) {
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+  res.header("Expires", "-1");
+  res.header("Pragma", "no-cache");
+  next();
+}
+
+router.get("/status", nocache, function(req, res) {
   res.sendStatus(200);
 });
 
-router.get("/login", passport.authenticate("auth0"), function(req, res) {
+router.get("/login", nocache, passport.authenticate("auth0"), function(
+  req,
+  res
+) {
   res.redirect("/");
 });
 
-router.get("/logout", function(req, res) {
+router.get("/logout", nocache, function(req, res) {
   req.logout();
   res.redirect("/");
 });
@@ -25,7 +35,7 @@ router.get(
   }
 );
 
-router.get("/failure", function(req, res) {
+router.get("/failure", nocache, function(req, res) {
   var error = req.flash("error");
   var error_description = req.flash("error_description");
   req.logout();
