a5fe516bb9efc8552a96379bba366b26019897d3
Kyle Parisi <kyle.parisi@robustwealth.com>
2018-05-10 12:56:24 -0400

    Set keys with dots

--- administration.js
+++ administration.js
@@ -28,7 +28,7 @@ app.get("/origins", (req, res) => {
 
 app.post("/origins/save", (req, res) => {
   const { regex_key, domain, ip } = req.body;
-  db.set(`origins.${regex_key}`, { domain, ip }).write();
+  db.set(`origins.["${regex_key}"]`, { domain, ip }).write();
   console.log(req.body);
   res.redirect("/");
 });
