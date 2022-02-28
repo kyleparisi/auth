f73d003e3c2f02b60db013cae0e634fe6010b268
Kyle Parisi <kyleparisi@gmail.com>
2022-02-27 20:42:12 -0500

    more tests

--- main.test.js
+++ main.test.js
@@ -2,6 +2,26 @@ require("./config/env");
 const request = require("supertest");
 const main = require("./main");
 
+const defaultConfig = {
+  session: {
+    driver: "memory"
+  },
+  strategies: {
+    google: {
+      clientID: "abc",
+      clientSecret: "def"
+    }
+  },
+  origins: {
+    "localhost:8080": {
+      domain: "google.com",
+      ip: undefined,
+      headers: {},
+      guard_emails: undefined
+    }
+  }
+};
+
 beforeEach(() => {
   const debug = require("debug")("app");
   global.debug = debug;
@@ -23,34 +43,39 @@ test("starts", () => {
       }
     }
   };
-  app = main(db);
+  const app = main(db);
   expect(app).toBeDefined();
 });
 
 test("/api/healthz", done => {
-  db = {
-    session: {
-      driver: "memory"
-    },
-    strategies: {
-      google: {
-        clientID: "abc",
-        clientSecret: "def"
-      }
-    },
-    origins: {
-      "localhost:8080": {
-        domain: "google.com",
-        ip: undefined,
-        headers: {},
-        guard_emails: undefined
-      }
-    }
-  };
-  app = main(db);
+  const app = main(defaultConfig);
   request(app)
     .get("/api/healthz")
     .expect(200)
+    .end(function(err, res) {
+      if (err) throw err;
+      done();
+    });
+});
+
+test("/api/user requires login", done => {
+  const app = main(defaultConfig);
+  request(app)
+    .get("/api/user")
+    .expect(302)
+    .expect("Location", "/login")
+    .end(function(err, res) {
+      if (err) throw err;
+      done();
+    });
+});
+
+test("/api/user/id requires login", done => {
+  const app = main(defaultConfig);
+  request(app)
+    .get("/api/user/id")
+    .expect(302)
+    .expect("Location", "/login")
     .end(function(err, res) {
       if (err) throw err;
       done();
